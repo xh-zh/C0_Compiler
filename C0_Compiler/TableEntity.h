a788983d22d7a6a1ff436c82b19ce8f1be217041
xh-zh <mr.xhzh@gmail.com>
2018-01-01 11:22:12 +0800

    新增地址描述符

--- C0_Compiler/TableEntity.h
+++ C0_Compiler/TableEntity.h
@@ -9,6 +9,7 @@ public:
 	int		value;		//符号的值
 	int		dim;		//非数组维数为0吧，函数则是参数个数，再给它赋予一个新的意义：为-1则代表是临时变量
 	string	parent;		//父函数名
+	vector<string> address_descriptor;//地址描述符，可包含寄存器、内存地址
 	TableEntity(void);
 	TableEntity(string name, int addr, enum _CLASS _class,
 		enum kinds type, int value, int dim, string parent);
